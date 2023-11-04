0b691c256658ddafa33aa534ebf1248f6b3dc0ae
Mathieu Stefani <auquetal@gmail.com>
2023-11-04 17:04:19 +0100

    Get rid of `concept` namespace in `result.h`
    In C++20, `concept` now became a keyword to define a concept. This commit simply moves the `EqualityComparable` trait outside of the `concept` namespace to get rid of it

--- include/fixpp/utils/result.h
+++ include/fixpp/utils/result.h
@@ -689,8 +689,6 @@ struct Constructor<void, E> {
 
 } // namespace details
 
-namespace concept {
-
 template<typename T, typename = void> struct EqualityComparable : std::false_type { };
 
 template<typename T>
@@ -702,9 +700,6 @@ typename std::enable_if<
 > : std::true_type
 {
 };
-
-
-} // namespace concept
 
 template<typename T, typename E>
 struct Result {
@@ -867,8 +862,8 @@ private:
 
 template<typename T, typename E>
 bool operator==(const Result<T, E>& lhs, const Result<T, E>& rhs) {
-    static_assert(concept::EqualityComparable<T>::value, "T must be EqualityComparable for Result to be comparable");
-    static_assert(concept::EqualityComparable<E>::value, "E must be EqualityComparable for Result to be comparable");
+    static_assert(EqualityComparable<T>::value, "T must be EqualityComparable for Result to be comparable");
+    static_assert(EqualityComparable<E>::value, "E must be EqualityComparable for Result to be comparable");
 
     if (lhs.isOk() && rhs.isOk()) {
         return lhs.storage().template get<T>() == rhs.storage().template get<T>();
@@ -880,7 +875,7 @@ bool operator==(const Result<T, E>& lhs, const Result<T, E>& rhs) {
 
 template<typename T, typename E>
 bool operator==(const Result<T, E>& lhs, types::Ok<T> ok) {
-    static_assert(concept::EqualityComparable<T>::value, "T must be EqualityComparable for Result to be comparable");
+    static_assert(EqualityComparable<T>::value, "T must be EqualityComparable for Result to be comparable");
 
     if (!lhs.isOk()) return false;
 
@@ -894,7 +889,7 @@ bool operator==(const Result<void, E>& lhs, types::Ok<void>) {
 
 template<typename T, typename E>
 bool operator==(const Result<T, E>& lhs, types::Err<E> err) {
-    static_assert(concept::EqualityComparable<E>::value, "E must be EqualityComparable for Result to be comparable");
+    static_assert(EqualityComparable<E>::value, "E must be EqualityComparable for Result to be comparable");
     if (!lhs.isErr()) return false;
 
     return lhs.storage().template get<E>() == err.val;
